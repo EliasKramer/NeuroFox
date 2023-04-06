09a8307d3bc705831ad087803f6fcf29463b7306
EliasKramer <kramer.elias@outlook.de>
2023-04-06 20:45:24 +0200

    reading mnist works

--- ConvolutionalNeuralNetwork/digit_data.cpp
+++ ConvolutionalNeuralNetwork/digit_data.cpp
@@ -6,9 +6,10 @@ digit_data::digit_data(const digit_image_t& data, const std::string& label)
 
 std::vector<digit_data> digit_data::get_digit_training_data()
 {
+
 	digit_image_collection_t training_data_mnist = load_mnist_data(
-		"/data/digit_recognition/train-images-idx3-ubyte",
-		"/data/digit_recognition/train-labels-idx1-ubyte");
+		"/data/digit_recognition/train-images.idx3-ubyte",
+		"/data/digit_recognition/train-labels.idx1-ubyte");
 
 	std::vector<digit_data> training_data;
 	return training_data;
@@ -17,8 +18,8 @@ std::vector<digit_data> digit_data::get_digit_training_data()
 std::vector<digit_data> digit_data::get_digit_testing_data()
 {
 	digit_image_collection_t testing_data_mnist = load_mnist_data(
-		"/data/digit_recognition/t10k-images-idx3-ubyte",
-		"/data/digit_recognition/t10k-labels-idx1-ubyte");
+		"/data/digit_recognition/t10k-images.idx3-ubyte",
+		"/data/digit_recognition/t10k-labels.idx1-ubyte");
 
 	std::vector<digit_data> testing_data;
 	return testing_data;
