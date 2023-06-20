ecf0aec300fd08ead155d7951d1491a2ffafe9f4
EliasKramer <kramer.elias@outlook.de>
2023-06-20 22:10:34 +0200

    stuff

--- util.cpp
+++ util.cpp
@@ -35,7 +35,12 @@ float random_float_excl(float min, float max)
 	if (ret_val < min || ret_val >= max)
 	{
 		//why does this get called TODO FIX
-		throw std::runtime_error("random_float_excl returned a value outside of the range");
+		std::cout
+			<< "random_float_excl returned a value outside of the range "
+			<< min << " to " << max << std::endl
+			<< "returned value: " << ret_val
+			<< std::endl;
+		return min;
 	}
 
 	return ret_val;
@@ -128,20 +133,33 @@ size_t convolution_output_size(
 
 std::string ms_to_str(size_t ms)
 {
-	//TODO make this work
-	size_t seconds = ms / 1000;
-	size_t minutes = seconds / 60;
-	size_t hours = minutes / 60;
+	size_t s = 1000;
+	size_t min = 60 * s;
+	size_t h = 60 * min;
+	size_t d = 24 * h;
+	size_t w = 7 * d;
+
+	size_t week_count = ms / w;
+	size_t day_count = (ms - week_count * w) / d;
+	size_t hour_count = (ms - week_count * w - day_count * d) / h;
+	size_t minute_count = (ms - week_count * w - day_count * d - hour_count * h) / min;
+	size_t second_count = (ms - week_count * w - day_count * d - hour_count * h - minute_count * min) / s;
+	size_t millisecond_count = ms - week_count * w - day_count * d - hour_count * h - minute_count * min - second_count * s;
 
 	std::string result = "";
-	if (hours > 0)
-		result += std::to_string(hours) + "h ";
-	if (minutes > 0)
-		result += std::to_string(minutes) + "m ";
-	if (seconds > 0)
-		result += std::to_string(seconds) + "s ";
-	if (ms > 0)
-		result += std::to_string(ms) + "ms ";
+
+	if (week_count > 0)
+		result += std::to_string(week_count) + "w ";
+	if (day_count > 0)
+		result += std::to_string(day_count) + "d ";
+	if (hour_count > 0)
+		result += std::to_string(hour_count) + "h ";
+	if (minute_count > 0)
+		result += std::to_string(minute_count) + "m ";
+	if (second_count > 0)
+		result += std::to_string(second_count) + "s ";
+	if (millisecond_count > 0)
+		result += std::to_string(millisecond_count) + "ms ";
 
 	return result == "" ? "0ms" : result;
 }
